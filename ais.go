b472ff5783226c9080832ff816865266c484941e
fcecin <fcecin@gmail.com>
2019-09-12 19:27:03 +0200

    initial commit

---
+++ ais.go
@@ -0,0 +1,23 @@
+/*
+
+   Alien Invasion Simulator
+
+   ais -gen MAPFILE MAXX MAXY CD RD
+
+   ais MAPFILE N
+
+   
+
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Alien Invasion Simulator!\n")
+   
+   
+}
